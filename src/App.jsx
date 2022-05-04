92ddb6b8f331b45577012f8f8e92cd55aa31b0be
BexFox <karis601293@gmail.com>
2022-05-04 16:35:16 +0900

    v1.2.0 Make a layout

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,15 @@ function App() {
     <div className='App'>
       <header className='App-header'>
         <h1>Choose the Tech Framework</h1>
+        <nav>
+          <ul className='btn-group'>
+            <li className='item'>
+              <a>React</a>
+            </li>
+            <li className='item'>Vue</li>
+            <li className='item'>Svelte</li>
+          </ul>
+        </nav>
       </header>
       <section className='contents'>
         <h2>contents</h2>
